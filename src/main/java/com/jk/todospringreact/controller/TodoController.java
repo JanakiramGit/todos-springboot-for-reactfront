052a8146fe0ec4c0644c200d727a8cd0f5df8939
Janakiram Sampalli <janakiram.sampalli@gmail.com>
2020-07-26 23:39:15 +1000

    CORS update

--- src/main/java/com/jk/todospringreact/controller/TodoController.java
+++ src/main/java/com/jk/todospringreact/controller/TodoController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jk.todospringreact.model.Todo;
 import com.jk.todospringreact.model.TodoRepository;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://jk-react-app.herokuapp.com")
 @RestController
 public class TodoController {
 	
